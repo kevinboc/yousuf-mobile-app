2e1bfa6c59a60add3514996b56fc21567147c11c
jordanlkfung <jordanlkfung@gmail.com>
2024-03-02 19:41:29 -0800

    riverpod state management

--- lib/chatscreen.dart
+++ lib/chatscreen.dart
@@ -1,70 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'chatscreen_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'message_model.dart';
 
-class ChatScreen extends StatelessWidget {
-  ChatScreen({super.key});
-  // final ChatScreenController = Get.find();
-  final ChatScreenController cScreenController =
-      Get.put(ChatScreenController());
-  final TextEditingController _textEditingController = TextEditingController();
-  void submitMessage() {
-    cScreenController.newUserMessage(_textEditingController.text);
-    _textEditingController.clear();
-  }
+// class Message extends StateNotifier<String> {
+//   Message() : super("");
+//   void setMessage(String m) => state = m;
+//   void removeMessage() => state = "";
+//   void incMessage() => state = "s";
+// }
+class MessageList extends StateNotifier<List<Message>> {
+  @override
+  MessageList() : super([]);
+  void addMessage(String text, bool fromUser) =>
+      state = [...state, Message(text, fromUser, TimeOfDay.now())];
+  List<Message> get messageList => state;
+}
 
+final messageListProvider =
+    StateNotifierProvider<MessageList, List<Message>>((ref) => MessageList());
+// final messageProvider = StateNotifierProvider((ref) => Message());
+
+class ChatScreen extends StatelessWidget {
+  final TextEditingController _textController = TextEditingController();
+
+  ChatScreen({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Chat"),
+          title: const Text("Chat"),
         ),
-        body: Column(
-          children: [
-            Expanded(child: _messageList()),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15, 0, 15, 30),
-              child: _inputField(),
-            ),
-          ],
-        ));
+        body: Column(children: [
+          Expanded(child: _messageListView()),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+            child: _textInputWidget(),
+          )
+        ]));
   }
 
-  Widget _inputField() {
-    return Row(children: [
-      Expanded(
-          child: TextField(
-        controller: _textEditingController,
+  Widget _textInputWidget() {
+    return Consumer(builder: ((context, ref, child) {
+      return TextField(
+        controller: _textController,
         decoration: InputDecoration(
             suffixIcon: IconButton(
-          onPressed: submitMessage,
-          icon: Icon(Icons.arrow_upward),
-          tooltip: "Send",
-          splashColor: Colors.green[400],
+          onPressed: () => ref
+              .read(messageListProvider.notifier)
+              .addMessage(_textController.text, true),
+          icon: const Icon(Icons.arrow_upward),
         )),
-      ))
-    ]);
+      );
+    }));
   }
 
-  Widget _messageList() {
-    return Obx(() => ListView.builder(
-          itemCount: cScreenController.messageList.length,
+  Widget _messageListView() {
+    return Consumer(
+      builder: (context, ref, child) {
+        final List<Message> messageList = ref.watch(messageListProvider);
+        return ListView.builder(
+          itemCount: messageList.length,
           itemBuilder: (context, index) {
-            return ListTile(
-                title: _messageItem(cScreenController.messageList[index]));
+            return ListTile(title: _messageItem(messageList[index]));
           },
-        ));
+        );
+      },
+    );
   }
+}
 
-  Widget _messageItem(Message displayMessage) {
-    var alignment = displayMessage.messageFromUser
-        ? Alignment.centerRight
-        : Alignment.centerLeft;
-    return Container(
-        alignment: alignment,
-        child: Column(
-          children: [Text(displayMessage.text)],
-        ));
-  }
+Widget _messageItem(Message content) {
+  var alignment =
+      content.messageFromUser ? Alignment.centerRight : Alignment.centerLeft;
+  return Container(
+    alignment: alignment,
+    child: Column(
+      children: [
+        Text(content.text),
+      ],
+    ),
+  );
 }
