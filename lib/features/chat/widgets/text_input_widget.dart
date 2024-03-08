faa22168f3207cbeb64077ffa356cf9d77d80da2
jordanlkfung <jordanlkfung@gmail.com>
2024-03-07 18:28:19 -0800

    code organization of chatscreen

---
+++ lib/features/chat/widgets/text_input_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yousuf_mobile_app/features/chat/pages/riverpod/message_list_provider.dart';
+
+class TextInputWidget extends ConsumerWidget {
+  TextInputWidget({super.key});
+  final TextEditingController _textController = TextEditingController();
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Consumer(builder: ((context, ref, child) {
+      return Container(
+        padding: const EdgeInsets.fromLTRB(20, 10, 20, 25),
+        height: 90,
+        child: TextField(
+          controller: _textController,
+          decoration: InputDecoration(
+            hintText: "Enter Message...",
+            filled: true,
+            fillColor: Colors.white10,
+            enabledBorder:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            focusedBorder:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+            suffixIcon: IconButton(
+              onPressed: () {
+                ref
+                    .read(messageListProvider.notifier)
+                    .addMessage(_textController.text, true);
+                _textController.clear();
+              },
+              icon: const Icon(Icons.arrow_upward),
+            ),
+          ),
+        ),
+      );
+    }));
+  }
+}
