5c698d9c6b0c06b8be3e298175a82c71707f112a
Rha02 <romuzus08@gmail.com>
2024-12-13 16:06:47 -0800

    Text To Speech

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tts/flutter_tts.dart';
+import 'package:logger/logger.dart';
 import 'package:yousuf_mobile_app/models/chat_message.dart';
+
+final Logger _logger = Logger();
 
 class MessageBubble extends StatefulWidget {
   const MessageBubble({required this.chatMessage, super.key});
@@ -12,6 +15,62 @@ class MessageBubble extends StatefulWidget {
 }
 
 class _MessageBubbleState extends State<MessageBubble> {
+  late FlutterTts flutterTts;
+  var playing = false;
+
+  Future<void> initTts() async {
+    flutterTts = FlutterTts();
+    // if ios, set shared instance to true
+    // if (Theme.of(context).platform == TargetPlatform.iOS) {
+    //   await flutterTts.setSharedInstance(true);
+    // }
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setSpeechRate(0.5);
+    await flutterTts.setVolume(1.0);
+    await flutterTts.setPitch(1.0);
+
+    flutterTts.setCompletionHandler(() {
+      if (mounted) {
+        setState(() {
+          playing = false;
+        });
+      }
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      _logger.e("error: $msg");
+      setState(() {
+        playing = false;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initTts();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
+  Future<void> toggleSpeech(String text) async {
+    if (playing) {
+      await flutterTts.stop();
+      setState(() {
+        playing = false;
+      });
+    } else {
+      await flutterTts.speak(text);
+      setState(() {
+        playing = true;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -57,8 +116,8 @@ class _MessageBubbleState extends State<MessageBubble> {
             if (!widget.chatMessage.isUser)
               IconButton(
                 color: Theme.of(context).primaryColor,
-                icon: Icon(Icons.play_arrow),
-                onPressed: () {},
+                icon: Icon(playing ? Icons.pause : Icons.play_arrow),
+                onPressed: () => toggleSpeech(widget.chatMessage.message),
               ),
           ],
         ),
